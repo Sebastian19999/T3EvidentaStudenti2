6947c5bc2a43edd9c7c811eab0559383727760c6
sebygalan <seby.galan@gmail.com>
2020-04-01 12:54:32 +0300

    Import project

---
+++ EvidentaStudenti2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace EvidentaStudenti2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student("Ionescu");
+            string c;
+            string noteCmd="";
+
+           do
+            {
+                Console.Clear();
+                Console.WriteLine("s: Citeste note de la tastatura ca un sir de caractere.");
+                Console.WriteLine("t: Citeste note de la tastatura (cate o nota pe rand).");
+                Console.WriteLine("c: Citeste note din linia de comanda");
+                Console.WriteLine("a: Afisare note");
+                Console.WriteLine("i: Info autor");
+                Console.WriteLine("x: Iesire");
+                {
+                    switch (c = Console.ReadLine())
+                    {
+                        case "s":
+                            Console.WriteLine("\n Citire tastatura ca un sir \n");
+                            Console.Write("Introduceti sirul de note: ");
+                            string sir = Console.ReadLine();
+                            s.Setnote(sir);
+                            Console.ReadLine();
+                            break;
+                        case "t":
+                            Console.WriteLine("Citeste note de la tastatura (cate o nota pe rand).");
+                            s.SetNote();
+
+                            Console.ReadLine();
+                            break;
+                        case "c":
+                            Console.WriteLine("c: Citeste note din linia de comanda");
+                            if (args.Length == 0)
+                                Console.Write("Linia de comanda nu contine argumente");
+                            else
+                            {
+                                for (int ind = 0; ind < args.Length-1; ind++)
+                                    noteCmd+=(string)(args[ind]+" ");
+                                noteCmd += args[args.Length - 1];
+                                s.Setnote(noteCmd);
+                                Console.WriteLine("Citirea din linia de comanda a fost realizata cu succes!");
+                            }
+                                //s.SetNote();
+                            Console.ReadLine();
+                            break;
+                        case "a":
+                            s.ConversieLaSir();
+                            Console.ReadLine();
+                            break;
+                        case "i":
+                            Console.WriteLine("\n Informatii autor: Galan Calin Sebastian 3123b");
+                            Console.ReadLine();
+                            break;
+                        case "x":
+                            return;
+                    }
+                }
+            } while (true);
+        }
+    }
+}
---
+++ EvidentaStudenti2/Student.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EvidentaStudenti2
+{
+    class Student
+    {
+
+            private int[][] note;
+            private string nume;
+            
+            static int p = 1;
+            // contructor implicit
+            public Student()
+            {
+            }
+
+            // constructor cu parametri
+            public Student(string _nume)
+            {
+                nume = _nume;
+            }
+
+            public void SetNote()
+            {
+                int n;
+                note= new int[4][];
+            for (int a = 0; a < 4; a++)
+                note[a] = new int[15];
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Cate note doriti pentru anul "+(i+1)+":");
+                
+                n = Convert.ToInt32(Console.ReadLine());
+                
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write("Nota " + (j+1)+": " );
+                    int r = -1;
+                    while (!(r > 0 && r < 11))
+                    {
+                        
+                        r = Convert.ToInt32(Console.ReadLine());
+                        if (!(r > 0 && r < 11))
+                            Console.Write("Nota gresita, introduceti din nou nota... ");
+
+
+                    }
+                    note[i][j]= r;
+                }
+            }
+
+            }
+            public void Setnote(string sir)
+            {
+                int i = 0, j = 0;
+                note = new int[4][];
+                
+                for(int a=0;a<4;a++)
+                    note[a] = new int[15];
+                //string[] _note = sir.Split(";");
+                string[] _note = sir.Split(" ");
+                p = sir.Length;
+                foreach (var nota in _note)
+                {
+
+                if (nota == ",")
+                {
+                    i++;
+                    j = 0;
+                    p -= 2;
+                }
+                else
+                {
+                    note[i][j] = Convert.ToInt32(nota);
+                    j++;
+                }
+            }
+            
+            }
+            
+            public void ConversieLaSir()
+            {
+            
+            Console.WriteLine(nume);
+                for (int i = 0; i < 4; i++)
+                {
+                    int n = i;
+                    n++;
+                    Console.Write("An{0}:", n);
+                    for (int j = 0; j < 15; j++)
+                        if(note[i][j]!=0)
+                            Console.Write(" "+note[i][j]);
+                    Console.Write("\n");
+                }
+            
+        }
+        }
+    }
+
